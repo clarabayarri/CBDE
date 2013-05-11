014d692423883b982d0ead02b9dec23975aaa77b
marijose <recogemoras@gmail.com>
2013-05-11 15:26:44 +0200

    query1 changed, not functional yet

--- Mongo-Normalized/src/QuerySet.java
+++ Mongo-Normalized/src/QuerySet.java
@@ -6,8 +6,6 @@ import java.util.List;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
-import com.mongodb.DBCollection;
-
 
 
 
@@ -38,58 +36,36 @@ public class QuerySet {
 	}
 
 	private BasicDBObject query1(DB database) {
-//		BasicDBObject query = 
-//			database.lineitem.aggregate([
-//		        {$match: {l_shipdate: {$lt: '01-MAY-13'}}},
-//				{$group: 
-//					{_id: {l_returnflag: "$l_returnflag", l_linestatus: "$l_linestatus"},
-//					l_returnflag: "$l_returnflag",
-//					l_linestatus: "$l_linestatus",
-//					sum_qty: {$sum: "$l_quantity"},
-//					sum_base_price: {$sum: "$l_extendedprice"},
-//					sum_disc_price: {$sum: "$l_extendedprice*(1-$l_discount)"},
-//					sum_charge: {$sum: "$l_extendedprice*(1-$l_discount)*(1+$l_tax)"},
-//					avg_qty: {$avg: "$l_quantity"},
-//					avg_price: {$avg: "$l_extendedprice"},
-//					avg_disc: {$avg: "$l_discount"},
-//					count_order: {$sum: 1}
-//					}},
-//				{$sort: {l_returnflag: 1, l_linestatus: 1}}
-//			]);
-		
 		BasicDBObject cmdBody = new BasicDBObject("aggregate", "lineitem");
-		DBCollection myColl = database.getCollection("lineitem");
 		ArrayList<BasicDBObject> pipeline = new ArrayList<BasicDBObject>(); 
-		
-		Calendar calendar = new GregorianCalendar(2013,5,1);
+		//els mesos comencen en 0
+		Calendar calendar = new GregorianCalendar(2013,03,30);
 		
 		// create our pipeline operations, first with the $match
-		BasicDBObject match = new BasicDBObject("$match", new BasicDBObject("$L_ShipDate", new BasicDBObject("$lt", new java.sql.Date(calendar.getTime().getTime()))));
+		BasicDBObject match = new BasicDBObject("$match", new BasicDBObject("L_ShipDate", new BasicDBObject("$lte", calendar.getTime())));
 
 		// Now the $group operation
 		BasicDBObject groupFields = new BasicDBObject( "L_ReturnFlag", "$L_ReturnFlag" );
 		groupFields.put( "L_LineStatus", "$L_LineStatus" );
+		groupFields.put("L_ShipDate", "$L_ShipDate");
 		groupFields = new BasicDBObject( "_id", groupFields);
-		
-//		groupFields.put("L_ReturnFlag", "$L_ReturnFlag");
-//		groupFields.put("L_LineStatus", "$L_LineStatus");
-		
+				
 		groupFields.put("sum_qty", new BasicDBObject( "$sum", "$L_Quantity"));
 		groupFields.put("sum_base_price", new BasicDBObject( "$sum", "$L_ExtendedPrice"));
-		groupFields.put("sum_disc_price", new BasicDBObject( "$sum", "$L_ExtendedPrice*(1-$L_Discount)"));
-		groupFields.put("sum_charge", new BasicDBObject( "$sum", "$L_ExtendedPrice*(1-$L_Discount)*(1+$L_Tax)"));
-		
+//		--------------------------------------------------------------------------------------------------------------------
+//		groupFields.put("sum_disc_price", new BasicDBObject( "$sum", "$L_ExtendedPrice*(1-$L_Discount)"));
+//		groupFields.put("sum_charge", new BasicDBObject( "$sum", "$L_ExtendedPrice*(1-$L_Discount)*(1+$L_Tax)"));
+//		--------------------------------------------------------------------------------------------------------------------
 		groupFields.put("avg_qty", new BasicDBObject( "$avg", "$L_Quantity"));
 		groupFields.put("avg_price", new BasicDBObject( "$avg", "$L_ExtendedPrice"));
 		groupFields.put("avg_disc", new BasicDBObject( "$avg", "$L_Discount"));
 		
 		groupFields.put("count_order", new BasicDBObject( "$sum", 1));
-		
+
 		BasicDBObject group = new BasicDBObject("$group", groupFields);
 		
 		//in sorting 1 is ASC -1 is DESC
-		BasicDBObject sortFields = new BasicDBObject( "L_ReturnFlag", 1 );
-		sortFields.put( "L_LineStatus", 1 );
+		BasicDBObject sortFields = new BasicDBObject( "_id", 1 );
 		
 		BasicDBObject sort = new BasicDBObject( "$sort", sortFields);
 		
@@ -98,9 +74,16 @@ public class QuerySet {
 	    pipeline.add(sort);
 	
 	    cmdBody.put("pipeline", pipeline);
-	    
-		// run aggregation
-		System.out.println(myColl.aggregate(match, group, sort));
+//	    DBCollection myColl = database.getCollection("lineitem");
+//	    AggregationOutput out = myColl.aggregate(match, group, sort);
+//		BufferedWriter writer = null;
+//		try {
+//			writer = new BufferedWriter(new FileWriter(
+//					"./aggregationOutput.txt"));
+//			writer.write(out.toString());
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
 		
 		return cmdBody;
 	}

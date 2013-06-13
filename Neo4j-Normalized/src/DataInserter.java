057c75fb59a84109fbc382edc48e970c75885dda
Clara Bayarri <nayami.ruka@gmail.com>
2013-06-13 16:05:30 +0200

    insert regions and nations

--- Neo4j-Normalized/src/DataInserter.java
+++ Neo4j-Normalized/src/DataInserter.java
@@ -1,8 +1,5 @@
 import java.io.File;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -12,7 +9,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
-import org.neo4j.graphdb.Direction;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Relationship;
@@ -31,19 +27,25 @@ public class DataInserter {
 	private static final String DB_PATH = "../neo4j-normalized-db";
 	
 	private List<Integer> regionIds = new ArrayList<Integer>();
+	private List<Integer> nationIds = new ArrayList<Integer>();
+	private List<Integer> partIds = new ArrayList<Integer>();
+	private List<Integer> supplierIds = new ArrayList<Integer>();
+	private Map<Integer, List<Integer>> partSuppIds = new HashMap<Integer, List<Integer>>();
+	private List<Integer> customerIds = new ArrayList<Integer>();
+	private List<Integer> orderIds = new ArrayList<Integer>();
+	private Map<Integer, Integer> lineItemIds = new HashMap<Integer, Integer>();
 	
     // START SNIPPET: vars
     GraphDatabaseService graphDB;
-    Node regionNode;
-    Node nationNode;
-    Node partNode;
-    Node supplierNode;
-    Node partSupplierNode;
-    Node customerNode;
-    Node orderNode;
-    Node lineItemNode;
     
-    List<Node> regionsNodes = new ArrayList<Node>();
+    private List<Node> regions = new ArrayList<Node>();
+	private List<Node> nations = new ArrayList<Node>();
+	private List<Node> parts = new ArrayList<Node>();
+	private List<Node> suppliers = new ArrayList<Node>();
+	private Map<Integer, List<Node>> partSupps = new HashMap<Integer, List<Node>>();
+	private List<Node> customers = new ArrayList<Node>();
+	private List<Node> orders = new ArrayList<Node>();
+	private List<Node> lineitems = new ArrayList<Node>();
     
     Relationship relationship;
     // END SNIPPET: vars
@@ -71,11 +73,15 @@ public class DataInserter {
 		Long timeDifference = endDate.getTime() - startDate.getTime();
 		System.out.println("Insertion took " + timeDifference + " milliseconds.\n");
 		
-		for ( int i = 0; i < regionsNodes.size(); ++i ) {
-			System.out.print( "R_RegionKey  ---  " + regionsNodes.get(i).getProperty( "R_RegionKey" ) + "\n" );
-			System.out.print( "R_Name  ---  " + regionsNodes.get(i).getProperty( "R_Name" ) + "\n" );
-			System.out.print( "R_Comment  ---  " + regionsNodes.get(i).getProperty( "R_Comment" ) + "\n" );
-			System.out.print( "skip  ---  " + regionsNodes.get(i).getProperty( "skip" ) + "\n\n\n" );
+		for ( int i = 0; i < regions.size(); ++i ) {
+			System.out.print( "R_RegionKey  ---  " + regions.get(i).getProperty( "R_RegionKey" ) + "\n" );
+			System.out.print( "R_Name  ---  " + regions.get(i).getProperty( "R_Name" ) + "\n" );
+			System.out.print( "R_Comment  ---  " + regions.get(i).getProperty( "R_Comment" ) + "\n" );
+			System.out.print( "skip  ---  " + regions.get(i).getProperty( "skip" ) + "\n" );
+			for (Relationship nation : regions.get(i).getRelationships()) {
+				System.out.println("\tRelated to " + nation.getEndNode().getProperty("N_NationKey"));
+			}
+			System.out.println();
 		}
 	}
 	
@@ -113,9 +119,10 @@ public class DataInserter {
 	private void insertRegions(Transaction tx) {
 		for ( int i = 1; i <= 5; ++i ) {
 			Integer id = getRandomInteger();
-			regionNode = graphDB.createNode();
+			Node regionNode = graphDB.createNode();
 			
 			while(regionIds.contains(id)) id = getRandomInteger();
+			regionIds.add(id);
 			
 			regionNode.setProperty( "R_RegionKey", id );
 			// Set one of the region names to the queried value
@@ -127,7 +134,31 @@ public class DataInserter {
 			regionNode.setProperty( "R_Comment", getRandomString(160));
 			regionNode.setProperty( "skip", getRandomString(64));
 			
-			regionsNodes.add( regionNode );
+			regions.add( regionNode );
+		}
+	}
+	
+	private void insertNations(Transaction tx) {
+		for ( int i = 1; i <= 5; ++i ) {
+			Node nationNode = graphDB.createNode();
+			
+			Integer id = getRandomInteger();
+			while(nationIds.contains(id)) id = getRandomInteger();
+			nationIds.add(id);
+			nations.add( nationNode );
+			
+			nationNode.setProperty("N_NationKey", id);
+			nationNode.setProperty("N_Name", getRandomString(64));
+			
+			// N_RegionKey
+			int index = random.nextInt(regionIds.size());
+			Node regionNode = regions.get(index);
+			Relationship relationship = regionNode.createRelationshipTo(nationNode, RelTypes.HAS_NATION);
+			
+			nationNode.setProperty("N_Comment", getRandomString(160));
+			nationNode.setProperty("skip", getRandomString(64));
+			
+			
 		}
 	}
 	
@@ -138,6 +169,7 @@ public class DataInserter {
         Transaction tx = graphDB.beginTx();
         try {
         	insertRegions(tx);
+        	insertNations(tx);
             tx.success();
         }
         finally {
--- Neo4j-Normalized/src/Neo4jNormalized.java
+++ Neo4j-Normalized/src/Neo4jNormalized.java
@@ -1,14 +1,3 @@
-import java.io.File;
-import java.io.IOException;
-
-import org.neo4j.graphdb.Direction;
-import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.Node;
-import org.neo4j.graphdb.Relationship;
-import org.neo4j.graphdb.RelationshipType;
-import org.neo4j.graphdb.Transaction;
-import org.neo4j.graphdb.factory.GraphDatabaseFactory;
-import org.neo4j.kernel.impl.util.FileUtils;
 
 
 public class Neo4jNormalized {
